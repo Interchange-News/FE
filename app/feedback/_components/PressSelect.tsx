3a6cf7ed9e620c2ea7b716ac65ee038e40aeb70f
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2025-04-05 21:47:26 +0900

    hotfix: 빌드 에러 수정

--- app/feedback/_components/PressSelect.tsx
+++ app/feedback/_components/PressSelect.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import Select from "react-select";
+import styles from "../page.module.css";
 
 interface PressOption {
   value: string;
@@ -20,16 +21,18 @@ export default function PressSelect({
   setSelectedPress,
 }: PressSelectProps) {
   return (
-    <Select
-      id="press"
-      options={options}
-      value={options.find((option) => option.value === selectedPress)}
-      onChange={(option) => setSelectedPress(option?.value || "")}
-      placeholder="언론사를 검색하세요"
-      isClearable
-      isSearchable
-      classNamePrefix="select"
-      noOptionsMessage={() => "검색 결과가 없습니다"}
-    />
+    <div className={styles.selectWrapper}>
+      <Select
+        id="press"
+        options={options}
+        value={options.find((option) => option.value === selectedPress)}
+        onChange={(option) => setSelectedPress(option?.value || "")}
+        placeholder="언론사를 검색하세요"
+        isClearable
+        isSearchable
+        classNamePrefix="select"
+        noOptionsMessage={() => "검색 결과가 없습니다"}
+      />
+    </div>
   );
 }
