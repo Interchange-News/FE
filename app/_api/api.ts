964fbd1f2f182a15fd002439531461b960a28c3a
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2025-04-08 23:38:15 +0900

    hotfix: 데이터 캐싱 문제 해결

--- app/_api/api.ts
+++ app/_api/api.ts
@@ -2,12 +2,14 @@ import { NewsClusters } from "../_types";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 async function fetchNewsClusters(): Promise<NewsClusters> {
-  const response = await fetch(`${BASE_URL}/news`);
+  const response = await fetch(`${BASE_URL}/news`, {
+    next: { revalidate: 60 },
+  });
 
   if (!response.ok) {
     throw new Error(`HTTP error! Status: ${response.status}`);
   }
-
+  console.log(response);
   const data: NewsClusters = await response.json(); // JSON 데이터를 변환
   return data;
 }
