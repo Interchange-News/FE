9f66e29c9102235f64515dbb968ad8fc455c6829
PARKGEONTAE <112490505+prkgnt@users.noreply.github.com>
2025-04-19 20:59:28 +0900

    fix: next.config.ts 수정

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["server.interchange.news", "localhost"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "server.interchange.news",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
 };
 
